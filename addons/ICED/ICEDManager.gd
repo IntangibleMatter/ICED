1ee9a5cfecc440752e044c66cd4f60e8d3f0f6e0
IntangibleMatter <47702559+IntangibleMatter@users.noreply.github.com>
2022-02-04 22:38:30 -0800

    IDfk

--- addons/ICED/ICEDManager.gd
+++ addons/ICED/ICEDManager.gd
@@ -5,3 +5,8 @@ class_name ICEDManager
 export var CutsceneObjects : Dictionary # "String": NodePath format
 
 
+func loadCutscene() -> void:
+	pass
+
+func playCutscene(cutscene: Array) -> void:
+	pass
--- addons/ICED/ICED_Dock.gd
+++ addons/ICED/ICED_Dock.gd
@@ -1,13 +1,21 @@
+tool
 extends Control
 
-onready var GraphEdit := $GraphEdit
-onready var AddNode := $GraphEdit/AddNode
+onready var graphEdit := $GraphEdit
+onready var addNode := $GraphEdit/AddNode
 
 var nodeTypes := {
-	0: preload("res://addons/ICED/nodes/NodeSay.tscn"),
-	1: preload("res://addons/ICED/nodes/NodeIfElse.tscn"),
-	2: preload("res://addons/ICED/nodes/NodePlayAnimation.tscn"),
-	3: preload("res://addons/ICED/nodes/NodePlaySound.tscn")
+	0: "Choice",
+	1: "Cinebars",
+	2: preload("res://addons/ICED/nodes/NodeIfElse.tscn"),
+	3: preload("res://addons/ICED/nodes/NodePlayAnimation.tscn"),
+	4: "PlayMusic",
+	5: preload("res://addons/ICED/nodes/NodePlaySound.tscn"),
+	6: preload("res://addons/ICED/nodes/NodeSay.tscn"),
+	7: "set property",
+	8: "set variable",
+	9: "Spawn",
+	10: "wait"
 }
 
 # Called when the node enters the scene tree for the first time.
@@ -18,13 +26,13 @@ func _ready():
 func _on_AddNode_index_pressed(index:int):
 	var node : GraphNode = nodeTypes[index].instance()
 	node.offset = get_local_mouse_position()
-	GraphEdit.add_child(node)
+	graphEdit.add_child(node)
 
 
 
 
 func _on_GraphEdit_gui_input(event:InputEvent):
 	if event is InputEventMouseButton and event.button_index == 2 and event.pressed:
-		AddNode.set_position(event.position)
-		AddNode.popup()
+		addNode.set_position(event.position)
+		addNode.popup()
 		print("AAAAAAAAAA")
